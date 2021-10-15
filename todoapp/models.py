15260189ce616464689fe61b14efc20732f2aba6
edwardtsbaum <edwardt.s.baum@gmail.com>
2021-10-15 16:27:52 -0400

    adding user capability

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref
 from todoapp import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
@@ -11,10 +12,15 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    task = db.relationship('Todo', backref='user', lazy=True)
+    #our task attribute has a relationship to the Todo model, the bacref is similar to adding 
+    #another column, the lazy argument defines when sql alchemy loads the data, we can use it get the 
+    #individual data for this user
     
 
     def __repr__(self):#how our object is printed whenever its printed out
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 # create model
 class Todo(db.Model):
@@ -22,3 +28,7 @@ class Todo(db.Model):
     task = db.Column(db.Text, nullable=False)
     duedate= db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)#each task needs a user
+
+    def __repr__(self):
+        return f"User('{self.task}', '{self.duedate}')"
--- todoapp/routes.py
+++ todoapp/routes.py
@@ -1,3 +1,4 @@
+from operator import attrgetter
 from flask import request, render_template, url_for, redirect, session, flash, jsonify
 from todoapp import app, db, bcrypt
 from todoapp.models import Todo, User
@@ -10,9 +11,12 @@ def index():
     incomplete = Todo.query.filter_by(complete=False).all()
     complete = Todo.query.filter_by(complete=True).all()
     form = TaskForm()
+    sorttasks = Todo.query.all()
+    form = TaskForm()
     
-    return render_template('index.html', incomplete=incomplete, complete=complete, form=form)
+    return render_template('index.html', incomplete=incomplete, complete=complete, form=form, sorttasks=sorttasks)
 
+#User system
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -21,10 +25,13 @@ def register():
     if request.method == 'POST' and form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in.', 'success')
-        return redirect(url_for('login', user=user))
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash('Your account has been created! You are now able to log in.', 'success')
+            return redirect(url_for('login', user=user))
+        except:
+            flash('There was an issue creating your account. Please try again.', 'danger')
     else:    
         return render_template('register.html', title='Register', form=form)
 
@@ -47,6 +54,13 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+
+#Task Functionality
 @app.route('/task/', methods=['GET', 'POST'])
 @login_required
 def new_task():
@@ -58,7 +72,7 @@ def new_task():
             db.session.commit()
             return redirect(url_for('index'))
         except:
-            return 'There was an issue adding your task'
+            flash('There was an issue adding your task', 'danger')
     else:
         return render_template('index.html', title='index', form=form)
     
@@ -110,6 +124,18 @@ def update(id):
         form.task.data = updatetask.task
         form.duedate.data = updatetask.duedate
     return render_template('update.html', title='Update Task', updatetask=updatetask, form=form)
+
+@app.route('/sort', methods=['GET','POST'])
+def sort():
+    sorttasks = Todo.query.all()
+    form = TaskForm()
+    
+    sorted(sorttasks, key=attrgetter('duedate'))
+    #sorted(sorttasks, key=lambda sorttasks: sorttasks.duedate)
+    db.session.commit()
+    flash('Your task has been sorted!', 'success')
+         
+    return redirect(url_for('index', sorttasks=sorttasks))
     
 
 @app.route("/about")
